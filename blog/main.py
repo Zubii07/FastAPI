a44fb78cc7f83f4e6c87baa227ecae1eefcfd685
Zubii07 <zohaibrasheed983@gmail.com>
2025-09-30 22:49:16 +0500

    Add update blog logic

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, Response, HTTPException 
 from . import schemas,models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -38,7 +38,20 @@ def get_blog(id: int, response: Response, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return {"detail": "Blog deleted"}
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, blog_post: schemas.BlogPost, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+    blog.update({'title': blog_post.title, 'body': blog_post.body})
+    db.commit()
+    return {"detail": "Blog updated"}
+
